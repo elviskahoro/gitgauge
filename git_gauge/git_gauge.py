7f2525d20dc79bef036f82b7408cd97eea4c718b
elvis kahoro <elvis@reflex.dev>
2024-11-02 17:34:32 -0700

    style: Renaming index page to be for vector search

--- git_gauge/git_gauge.py
+++ git_gauge/git_gauge.py
@@ -2,7 +2,7 @@
 import reflex as rx
 
 from .app_style import Style as AppStyle
-from .pages.project_tracker.page import index
+from .pages.project_tracker.page_vector_search import index
 from .pages.project_tracker.state import State
 
 APP_STYLE: AppStyle = AppStyle()
